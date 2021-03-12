37f3a349f0076ac114027137f39028f4bccca815
Clayton Cooper <claycooper8@gmail.com>
2021-03-12 18:33:05 -0500

    fixing up dumb admin bar

--- wp-content/themes/Random/front-page.php
+++ wp-content/themes/Random/front-page.php
@@ -1,19 +1,17 @@
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Random</title>
-	<link rel="stylesheet" href="/build/tailwind.css">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Random</title>
+		
+		<?php wp_head();?>
+		
 
-    <?php
-    wp_head();
-    ?>
-
-</head>
+	</head>
 
 
-<body class="bg-gray-500">
-	<p class="bg-blue-500 text-center">Hello World</p>
-</body>
+	<body class="bg-gray-500 ">
+		<p class="bg-blue-500 text-center">Hello World</p>
+	</body>
 </html>
--- wp-content/themes/Random/functions.php
+++ wp-content/themes/Random/functions.php
@@ -1,11 +1,34 @@
 <?php
 
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+							// This function makes it so that we can load the CSS/Tailwind in automatically
 function random_register_styles(){
-	wp_enqueue_style('random-style', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+
+	$version = wp_get_theme()->get( 'Version' );
+	wp_enqueue_style('random-style', get_template_directory_uri() . "/style.css", array('random-tailwind'), $version, 'all');
 	// this links to the tailwind stuff with   "/build/tailwind.css"
 	wp_enqueue_style('random-tailwind', get_template_directory_uri() . "/build/tailwind.css", array(), '1.0', 'all'); 
 
 }
 
 add_action( 'wp_enqueue_scripts', 'random_register_styles');
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+							// This removeds the stupid admin bar that was originally in the code
+function hide_admin_bar_from_front_end(){
+	if (is_blog_admin()) {
+	  return true;
+	}
+	return false;
+  }
+  add_filter( 'show_admin_bar', 'hide_admin_bar_from_front_end' );
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+
 ?>
+

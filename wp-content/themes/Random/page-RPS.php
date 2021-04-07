d267cb084adf5d518b6022d05571edf588ec98dc
Clayton Cooper <Claycooper8@gmail.com>
2021-04-07 12:11:40 -0400

    images are located in the file now

--- wp-content/themes/Random/page-RPS.php
+++ wp-content/themes/Random/page-RPS.php
@@ -1,12 +1,12 @@
 <?php include "header.php";?>
 
 	<header>
-		<h1>Rock Paper Scissors</h1>
+		<h1 class="text-center text-2xl my-5">Rock Paper Scissors</h1>
 	</header>
 
 
 	<!-- This is the score board -->
-	<div class="">
+	<div class="border border-solid border-white w-48 mx-auto">
 		<div id="user-label">User</div>
 		<div id="computer-label">Comp</div>
 		<span id="user-score">0</span>:<span id="computer-score">0</span>
@@ -16,15 +16,16 @@
 		<p>Paper covers rock. You Win!</p>
 	</div>
 
-	<div>
-		<div id="r">
-			<img src="images/rock.png" alt="">
+	<!-- These are the choices you can pick from -->
+	<div class="flex space-x-4 justify-center">
+		<div id="r" class="flex-0">
+			<img src="<?=get_template_directory_uri()?>/images/rock.png" alt="Rock" width="150">
 		</div>
-		<div id="p">
-			<img src="images/paper.png" alt="">
+		<div id="p" class="flex-0">
+			<img src="<?=get_template_directory_uri()?>/images/paper.png" alt="Paper" width="150">
 		</div>
-		<div id="s">
-			<img src="images/scissors.png" alt="">
+		<div id="s" class="flex-0">
+			<img src="<?=get_template_directory_uri()?>/images/scissors.png" alt="Scissors" width="150">
 		</div>
 	</div>
 

33e7b5583e236735215451e15b9049f7318d89ec
Clayton Cooper <Claycooper8@gmail.com>
2021-04-16 21:08:04 -0400

    fixed views for the header

--- wp-content/themes/Random/header.php
+++ wp-content/themes/Random/header.php
@@ -8,12 +8,12 @@
 		<?php wp_head();?> 
 		<!--^^^^^  this is loads all the function.php stuff into the page ^^^^^ -->     
 	</head>
-	<body class="bg-gray-500">
-		<div class="h-16 bg-blue-700 flex sm:px-6 lg:px-8">
-			<p class="flex-1 text-white text-left font-bold text-4xl pt-4 pl-3 tracking-wider justiify-start">Clayton Cooper</p>
+	<body class="bg-gray-600">
+		<div class="h-16 bg-blue-700 flex  sm:px-6 lg:px-8">
+			<p class="flex-1 sm::text-lg md:text-2xl lg:text-3xl xl:text-4xl text-white text-left font-bold pt-4 pl-3 tracking-wider justiify-start">Clayton Cooper</p>
 			<headng>
-				<h1 class="flex-1 text-white text-left font-bold text-4xl pt-4 pl-3 tracking-wider justiify-between"><?php the_title(); ?></h1>
+				<h1 class="flex-1 sm::text-lg md:text-2xl lg:text-3xl xl:text-4xl text-white text-left font-bold pt-4 pl-3 tracking-wider justiify-between"><?php the_title(); ?></h1>
 			</headng>
-			<p class="flex-1 text-white text-right font-bold text-4xl pt-4 pr-3 tracking-wider justify-end">Random Stuff</p>
+			<p class="flex-1 sm::text-lg md:text-2xl lg:text-3xl xl:text-4xl text-white text-right font-bold pt-4 pr-3 tracking-wider justify-end">Random Stuff</p>
 		</div>
 		<?php include 'nav.php';?>
--- wp-content/themes/Random/nav.php
+++ wp-content/themes/Random/nav.php
@@ -60,27 +60,5 @@
       <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
       <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
     </div>
-    <div class="pt-4 pb-3 border-t border-gray-700">
-      <div class="flex items-center px-5">
-        <div class="flex-shrink-0">
-          <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=QHjXeLsSa4&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-        </div>
-        <div class="ml-3">  
-          <div class="text-base font-medium text-white">Tom Cook</div>
-          <div class="text-sm font-medium text-gray-400">tom@example.com</div>
-        </div>
-        <button class="ml-auto flex-shrink-0 bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-          <span class="sr-only">View notifications</span>
-          <!-- Heroicon name: outline/bell -->
-          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-          </svg>
-        </button>
-      </div>
-      <div class="mt-3 px-2 space-y-1">
-        <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Your Profile</a>
-        <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Settings</a>
-        <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Sign out</a>
-      </div>
-    </div>
   </div>
 </nav>

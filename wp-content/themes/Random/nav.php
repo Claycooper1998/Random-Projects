de3bbcc2893b4397a3255948e7a7154b2a81e6b6
Clayton Cooper <Claycooper8@gmail.com>
2021-04-17 20:46:40 -0400

    new php page for new stuff to learn

--- wp-content/themes/Random/nav.php
+++ wp-content/themes/Random/nav.php
@@ -12,7 +12,7 @@
 
 
 
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+            <a href="/php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">PHP stuff</a>
 
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
           </div>
---
+++ wp-content/themes/Random/page-php.php
@@ -0,0 +1,6 @@
+<?php include "header.php";?>
+
+
+    <header>
+		<h1 class="text-center text-white text-2xl my-5">This is where everything from W3schools for php will be posted</h1>
+	</header>

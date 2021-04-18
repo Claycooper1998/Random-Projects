8a52b1f2dd170832d1368b844b2408248193bd3c
Clayton Cooper <Claycooper8@gmail.com>
2021-04-17 21:30:43 -0400

    started styling

--- wp-content/themes/Random/page-php.php
+++ wp-content/themes/Random/page-php.php
@@ -4,3 +4,12 @@
     <header>
 		<h1 class="text-center text-white text-2xl my-5">This is where everything from W3schools for php will be posted</h1>
 	</header>
+
+
+    <div class="text-white text-xl px-6 container pl-20 pt-6">
+        <h1 class="font-bold text-2xl">This is the "Home" section</h1>
+        <ol class="list-decimal pt-4">
+            <li></li>
+            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
+        </ol>
+	</div>

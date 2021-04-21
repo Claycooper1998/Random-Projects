cec756b09eb6983502f6a5c675469ecf742d6f26
Clayton Cooper <Claycooper8@gmail.com>
2021-04-20 22:04:20 -0400

    started the PHP content

--- wp-content/themes/Random/page-php.php
+++ wp-content/themes/Random/page-php.php
@@ -6,10 +6,33 @@
 	</header>
 
 
-    <div class="text-white text-xl px-6 container pl-20 pt-6">
-        <h1 class="font-bold text-2xl">This is the "Home" section</h1>
-        <ol class="list-decimal pt-4">
-            <li></li>
-            <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit</li>
-        </ol>
-	</div>
+    <div class="text-white text-center w-full bg-gray-500">
+        <h1 class="text-xl py-2">Intro</h1>
+        <div class="bg-gray-400 p-1">
+            <p class="text-black">&lt;?php echo "My first PHP script!"; ?&gt;</p>
+            <p>PHP stands for "Hypertext Preprocessor"</p>
+            <p>the scripts are executed on the server</p>
+            <p>PHP is the core of wordpress</p>
+            <h1 class="font-bold text-black">PHP files</h1>
+                <p>contain: text, HTML, CSS, JavaScript, and PHP code</p>
+                <p>The code is excecuted on the server and it returned as plain HTML, images, PDF files, flash movies, and text on the browser</p>
+            <h1 class="font-bold text-black">What can it do?</h1>
+                <p>- can generate dynamic page content</p>
+                <p>- create, open, read, write, delete, and close files on the server</p>
+                <p>- Collect form data</p>
+                <p>- send and receive cookies</p>
+                <p>- add, delete, modify data in your database</p>
+                <p>- control user access</p>
+                <p>- encrypt data</p>
+            <h1 class="font-bold text-black">Why PHP</h1>
+                <p>- runs on almost all platforms (windows, linux, unix, mac os x)</p>
+                <p>- compatable with almost all servers</p>
+                <p>- supports almost all databases</p>
+                <p>- easy to learn and runs efficiently on the server side</p>
+        </div>
+        <h1 class="text-xl py-2">Install</h1>
+        <div class="bg-gray-400 p-1">
+        </div>
+        
+        
+    </div>

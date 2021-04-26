26b4897f57513da5b161f411c106244bcb2cb088
Clayton Cooper <Claycooper8@gmail.com>
2021-04-26 09:49:26 -0400

    added php stuff

--- wp-content/themes/Random/page-php.php
+++ wp-content/themes/Random/page-php.php
@@ -40,8 +40,37 @@
             <p>This will display "Hello World" as regular text. Just make sure to end it with a semicolon ;</p>
             <h1 class="font-bold text-black">PHP Case Sensitivity</h1>
             <p>- Keywords, classes, functions, and user-defined functions like: if, else, while, echo, etc. are <span class="text-black font-semibold">NOT</span> case-sensitive</p>
-            
+            <p>- All varible names are case-sensitive (variables are created by the user with a "$name"</p>
         </div>
-        
-        
+        <h1 class="text-xl py-2">Comments</h1>
+        <div class="bg-gray-400 p-1">
+            <p>- the comment is not a excexcuted piecec of code, it is just for a programmer to see, or someone looking at the code</p>
+            <p>- Comments are good for other to understand your code, and to remind you what the code is doing or what it's for</p>
+            <p> ways to right comments:</p>
+            <p> "// to comments a single line"</p>
+            <p> "# This is also a single-line comment"</p>
+            <p> "/* This is a multiple-lines comment block that spans over multiple lines */</p>
+            <p> // You can also use comments to leave out parts of a code line $x = 5 /* + 15 */ + 5; echo $x;</p>
+        </div>
+        <h1 class="text-xl py-2">Variables</h1>
+        <div class="bg-gray-400 p-1">
+            <p>- Variables are "containers" for storing information to be used somewhere else</p>
+            <p>- in PHP, a variable starts/delcared with "$" followed by the name of the variable</p>
+            <p>- &lt;?php $txt = "hello world!"; ?></p>
+            <p>- &lt;?php echo $txt; ?></p>
+            <p>- this will display "Hello world!"</p>
+        </div>
+        <h1 class="text-xl py-2">Output variables</h1>
+        <div class="bg-gray-400 p-1">
+            <p>"echo" is often used to output the data on the screen</p>
+        </div>
+        <h1 class="text-xl py-2">Variable Scopes</h1>
+        <div class="bg-gray-400 p-1">
+            <p>Just like in all other languages, PHP has scores that can be local, global, and static</p>
+            <p>- A variable declared outside a function has a GLOBAL SCOPE and can only be accessed outside a function</p>
+            <p>- A variable declared within a function has a LOCAL SCOPE and can only be accessed within that function</p>
+            <p> the word "global" can be used inside of a function to be able to use a a variable from outside</p>
+            <p>- Naturally, PHP holds all global variables in an array call $GLOBALS[index] and can access any global variable by using $GLOBALS["name"]</p>
+            <p>- The Scope static helps make sure no variables are deleted after the execution of the code</p>
+        </div>
     </div>
